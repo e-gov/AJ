6060b78a900adf37ad03bdd650d2178e7bd7a05b
Ivo Mehide <ivo@degeetia.ee>
2016-03-14 20:41:34 +0200

    Tühi salvestaja kood

---
+++ salvestaja/src/main/java/ee/degeetia/xrtracker/salvestaja/Test.java
@@ -0,0 +1,26 @@
+package ee.degeetia.xrtracker.salvestaja;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public Test() {
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/plain");
+		ServletOutputStream os = resp.getOutputStream();
+		os.println("Hello world!");
+		os.flush();
+		os.close();
+	}
+}
