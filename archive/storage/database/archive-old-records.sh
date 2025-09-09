a435bae014f8acbe28f608d296fcb82b7eeb9945
sipsu1 <rsoggar@gmail.com>
2025-09-09 11:33:00 +0300

    Delete archive directory

--- archive/storage/database/archive-old-records.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Usage:
-# archive-old-records.sh {date}
-#
-# {date} should be in format 'YYYY-MM-DD'
-# Records are copied to STDOUT, redirect this to file if needed.
-# Set PosgreSQL environment variables PGDATABASE, PGHOST, PGPORT, PGUSER if needed!
-psql -c "copy (select * from ajlog where logtime < to_date('$1', 'YYYY-MM-DD')) to STDOUT"
--- archive/storage/database/delete-old-records.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Usage:
-# archive-old-records.sh {date}
-#
-# {date} should be in format 'YYYY-MM-DD'
-# Set PosgreSQL environment variables PGDATABASE, PGHOST, PGPORT, PGUSER if needed!
-psql -c "delete from ajlog where logtime < to_date('$1', 'YYYY-MM-DD')"
